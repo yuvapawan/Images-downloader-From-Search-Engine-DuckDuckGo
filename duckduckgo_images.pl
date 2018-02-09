18fedf4c53a44b43299251349a2230643f9384d8
yuvapawan <36308974+yuvapawan@users.noreply.github.com>
2018-02-10 03:26:36 +0530

    Add files via upload

---
+++ duckduckgo_images.pl
@@ -0,0 +1,49 @@
+use strict;
+use LWP::Simple;
+my $page=1;
+my $temp=0;
+my $cnt=0;
+my $count=50;
+my $id;
+print "Your search please: ";
+my $search = <STDIN>;
+chomp $search;
+$search=~s/\s/%20/isg;
+my $content = get("https://duckduckgo.com/?q=$search&t=hf&iax=images&ia=images");
+if($content=~/vqd='(.*?)';/is)
+{
+	$id=$1;
+}
+my $content1 = get("https://duckduckgo.com/i.js?l=wt-wt&o=json&q=$search&vqd=$id&f=,,,&p=0");
+&download_images($content1);
+sub download_images
+{
+	my $content1=shift;
+	while($content1=~m/"image":"(.*?)"/isg)
+	{
+        	my $image_url=$1;
+		print "$image_url\n";
+		my $image=$image_url;
+		while($image=~/\//isg)
+		{
+			$image=~s/.*?\///isg;
+		}
+		$image="/home/nambiaruran/Desktop/Pawan/logs/"."$image";
+		getstore($image_url, $image);
+		$cnt++;
+	}
+	if($temp<$cnt)
+        {
+             $temp=$cnt;
+             &download_pages();
+        }
+}
+sub download_pages
+{
+	my $next_url="https://duckduckgo.com/i.js?q=$search&p=1&s=$count&u=yahoo&f=,,,&l=wt-wt&vqd=$id";
+	$page++;
+	$count=$count+50;
+	print "\n$page\n\n";
+	my $next_content = get($next_url) or die 'Unable to get page';
+	&download_images($next_content);
+}
